b19d3a299f51704ccb2e97d1ecca42882d4eb933
翁治平 <gocreating@gmail.com>
2018-09-24 13:55:02 +0800

    06-05 finish

---
+++ src/providers/rest/rest.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+/*
+  Generated class for the RestProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class RestProvider {
+
+  constructor(public http: HttpClient) {
+    // console.log('Hello RestProvider Provider');
+  }
+
+  private getUrlReturn(url: string): Observable<string[]> {
+    return this.http.get(url)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return JSON.parse(body) || {};
+  }
+
+  private handleError(error: Response | any) {
+    let errMsg: string ;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+}
